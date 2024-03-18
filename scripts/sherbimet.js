ab21ac39fc3a2e989d5776108e21abfd303d7465
RamadaniEdon <92359718+RamadaniEdon@users.noreply.github.com>
2024-03-19 01:31:08 +0200

    Translate and resize carousel images

--- scripts/sherbimet.js
+++ scripts/sherbimet.js
@@ -1,31 +1,15 @@
 window.onload = function () {
   const children = document.getElementById("sherbimet").querySelectorAll("*");
-  const iframe = document.getElementById("television");
-  const startingInfo = document.getElementById("startingInfo");
-  
-  const handler = function() {
-    startingInfo.classList.add("displayNone");
-    startingInfo.classList.remove("displayBlock");
-    iframe.classList.remove("displayNone");
-    iframe.classList.add("displayBlock");
 
-    for(let j = 0; j < children.length; j++){
-      children[j].removeEventListener("click", handler);
-    }
-  }
-
-  for (let i = 0; i < children.length; i++) {
-    children[i].addEventListener("click", handler);
-  }
-
+  document.getElementById('television').style.visibility = 'visible';
 
   var visibleSideBar = false;
   const sideBar = document.getElementById("sherbimeContainer");
   const sideBarButton = document.getElementById("showSideBarBtn");
-  const handler2 = function(){
-    if(window.innerWidth > 768) return;
-    
-    if(!visibleSideBar){
+  const handler2 = function () {
+    if (window.innerWidth > 768) return;
+
+    if (!visibleSideBar) {
       sideBar.classList.add("moveRight");
       sideBar.classList.remove("moveLeft");
       sideBarButton.classList.add("rotate180");
@@ -39,11 +23,11 @@ window.onload = function () {
     }
   }
   sideBarButton.addEventListener("click", handler2);
-  for(let i = 0; i < children.length; i++){
+  for (let i = 0; i < children.length; i++) {
     children[i].addEventListener("click", handler2);
   }
 
-  document.addEventListener('click', function(event) {
+  document.addEventListener('click', function (event) {
     if (visibleSideBar && !sideBarButton.contains(event.target) && !sideBar.contains(event.target)) {
       handler2();
     }
