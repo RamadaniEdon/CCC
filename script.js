9eb4be48b4a3773fc93f8cdc8a0b61e962fbf522
OsmaniGent <gosmani@york.citycollege.eu>
2024-03-19 02:11:46 +0200

    Add Loading Animation

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+    const mainLogo = document.getElementById('main-logo');
+    mainLogo.addEventListener('animationend', function() {
+      // Hide the main logo
+      mainLogo.style.display = 'none';
+  
+      // Start animations for each 'C' part
+      const cParts = document.querySelectorAll('.c-part');
+      for (let i = 0; i < cParts.length; i++) {
+        cParts[i].style.display = 'block';
+        cParts[i].style.animation = `fly-off-${i+1} 1.5s forwards`;
+      }
+  
+      // After the 'C' parts have flown off the screen, display main content
+      setTimeout(() => {
+        const mainContent = document.getElementById('main-content');
+        mainContent.style.display = 'block';
+        mainContent.style.animation = 'fadeInContent 1s forwards';
+        // Set the body background back to the original color or make it transparent
+        document.body.style.backgroundColor = 'transparent';
+      }, 500); // This timeout should match the longest 'fly-off' animation duration
+    });
+  });
+  
