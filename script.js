14951b68652b07525c8088279290a95492ac979d
OsmaniGent <gosmani@york.citycollege.eu>
2024-03-20 14:37:01 +0200

    Add Some Changes That Were Required
    Add Image to About Us and Rreth Nesh
    Remove Carousel Text
    Change Animation
    Change Background Color(in progress)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
 
-  if (!sessionStorage.getItem('animationPlayed')) {
+  if (!sessionStorage.getItem('animationPlayed') && (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('/') || window.location.pathname.endsWith('home.html'))) {
 
     const mainLogo = document.getElementById('main-logo');
 
@@ -27,15 +27,27 @@ window.addEventListener('load', function() {
   } 
   else {
 
-    document.getElementById('main-logo').style.display = 'none';
-    const cParts = document.querySelectorAll('.c-part');
-    cParts.forEach(part => part.style.display = 'none');
+    const mainLogo = document.getElementById('main-logo');
 
-    const mainContent = document.getElementById('main-content');
-    mainContent.style.display = 'block';
-    mainContent.style.opacity = 1;
-    document.body.style.backgroundColor = '#f1e8e3';
+    mainLogo.style.animation = 'spin 1s linear forwards';
 
+    mainLogo.addEventListener('animationend', function() {
+
+      mainLogo.style.display = 'none';
+      const cParts = document.querySelectorAll('.c-part');
+
+      for (let i = 0; i < cParts.length; i++) {
+        cParts[i].style.display = 'block';
+        cParts[i].style.animation = `fly-off-${i+1} 1s forwards`;
+      }
+
+      setTimeout(() => {
+        const mainContent = document.getElementById('main-content');
+        mainContent.style.display = 'block';
+        mainContent.style.animation = 'fadeInContent 1s forwards';
+        document.body.style.backgroundColor = '#043d70';
+      }, 500);
+    });
   }
 
 });
